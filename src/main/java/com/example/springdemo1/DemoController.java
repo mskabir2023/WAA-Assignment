44a2e1026c643d9b4a735c59b52e59525d2ee32b
Mohammad Serajul Kabir Chy Rubel <135068982+mskabir2023@users.noreply.github.com>
2023-10-02 18:06:54 -0500

    first assignment

---
+++ src/main/java/com/example/springdemo1/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.springdemo1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @RequestMapping
+    public int demo(){
+        return 55;
+    }
+}
